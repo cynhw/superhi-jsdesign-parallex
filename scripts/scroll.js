286e510b33836991f92d95ac26674a58f0064edc
cynhw <cynhowong@gmail.com>
2019-05-25 16:42:57 -0700

    javascript file to start the parallex scroll effect

---
+++ scripts/scroll.js
@@ -0,0 +1,9 @@
+const pixelsTag = document.querySelector("div.pixels");
+
+// when we scroll the page, update the pixels tag to be how far we've scrolled
+
+document.addEventListener("scroll", function() {
+  
+  const pixels = 
+
+});
